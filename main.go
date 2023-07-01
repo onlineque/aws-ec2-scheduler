31bdcd16022d928fc8eacefcae69ae9448c0ad34
Vladimir Siman <online@clew.cz>
2023-07-01 21:28:56 +0200

    attempt to fix the missing region setup, hardcoded for now

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func Log(l *log.Logger, msg string) {
 }
 
 func getConfig(l *log.Logger) aws.Config {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
 	if err != nil {
 		Log(l, err.Error())
 		os.Exit(1)
